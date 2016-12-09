96b05bfa98f5f6fc1d93aab9aeeb8db00ac723c5
vladimirmian <1909582621@qq.com>
2016-12-10 01:35:55 +0800

    modifiled

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ let gulp = require('gulp'),
     proxy = require('http-proxy-middleware');
 const root = 'blog';
 const build = 'wwww';
-var paths = {
+const paths = {
     index: root + '/index.html',
     bowerJs: [path.join(root, 'bower_components/**/*.min.js'), path.join(root, 'bower_components/**/*-min.js')],
     bowerStyle: [path.join(root, 'bower_components/**/*.min.css'), path.join(root, 'bower_components/**/**/*.min.css')],
@@ -30,7 +30,7 @@ gulp.task('clean', function() {
     return gulp.src(paths.devCss)
         .pipe($.clean());
 });
-gulp.task('style',['clean'], function() {
+gulp.task('style', ['clean'], function() {
     return gulp.src(paths.devScss)
         .pipe($.sass(sassOptions).on('error', $.sass.logError))
         .pipe(gulp.dest(path.join(root, 'style')));
@@ -58,13 +58,22 @@ gulp.task('inject-style', ['style'], function() {
 });
 
 gulp.task('watch', ['inject-all'], function() {
-    console.log(paths.devScss)
-    gulp.watch(paths.devScss, function(cb) {
+    //没有安装那个compass的情况下用这个
+    /*gulp.watch(paths.devScss, function(cb) {
         if (isOnlyChange(cb)) {
             compileSingle(cb)
                 .pipe(browserSync.stream());
         } else {
             gulp.start('inject-style');
+        }
+    });*/
+    //使用那个compass的时候
+    gulp.watch(paths.devCss, function(cb) {
+        if (isOnlyChange(cb)) {
+            gulp.src(cb.path)
+                .pipe(browserSync.stream());
+        } else {
+            reInjectStyle();
         }
     });
     gulp.watch(paths.devScript, function(cb) {
@@ -78,6 +87,29 @@ gulp.task('watch', ['inject-all'], function() {
         }
     });
 });
+// use the compass to prod
+gulp.task('serve', ['watch'], function() {
+    browserSync.init({
+        port: 9105,
+        startPath: '/',
+        //open: 'local',
+        server: {
+            baseDir: root,
+            index: 'index.html',
+            /*middleware: proxy('/api', {
+                target: 'http://tmct.73go.cn/api/',
+                changeOrigin: true
+            })*/
+        },
+        brower: 'default'
+    });
+});
+
+
+
+
+
+
 
 function isOnlyChange(cb) {
     return cb.type === 'changed';
@@ -87,5 +119,12 @@ function compileSingle(cb) {
     return gulp.src(cb.path)
         .pipe($.sass(sassOptions).on('error', $.sass.logError))
         .pipe(gulp.dest(path.join(root, 'style')))
+        .pipe(browserSync.stream());
+}
+
+function reInjectStyle() {
+    return gulp.src(paths.index)
+        .pipe($.inject(gulp.src(paths.devCss), injectOption.dev))
+        .pipe(gulp.dest(root))
         .pipe(browserSync.stream());
 }
